8c00a52335336aac41c601ba4d7ad86ff76658d9
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-11-27 00:19:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Problems_of_multiple_class_inheritance1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
